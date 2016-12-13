e0dbf5185325935a0f81a61f227e206732541839
andyding1 <andy.ding89@gmail.com>
2016-12-13 05:50:10 -0500

    added all API calls for current workflows

--- js/script.js
+++ js/script.js
@@ -41,6 +41,7 @@ var eoAPI = {
             <input name="documentFileName" type="text" placeholder="documentFileName"/> \
             <p>srcFile:</p> \
             <input name="srcFile" type="file"/> \
+            <p>mimeType:</p> \
             <select name="mimeType"> \
               <optgroup label="Documents"> \
                 <option value="application/pdf" selected="selected"> \
@@ -165,7 +166,7 @@ var eoAPI = {
         <div class="form"> \
           <h4>Parameters</h4> \
           <form action="" target="responseFrame3" method="post" enctype="multipart/form-data" name="submitForm3" id="submitForm3"> \
-            <p>instructionsXML</p> \
+            <p>instructionsXML:</p> \
             <input type="file" name="instructionsXML" /> \
             <button type="submit" form="submitForm3" onclick="redirectAction(document.submitForm3,\'?action=eoConfigureSortOrder\', parent.document.getElementById(\'hostname\').innerHTML)">submit</button> \
           </form> \
@@ -189,7 +190,7 @@ var eoAPI = {
         <div class="form"> \
           <h4>Parameters</h4> \
           <form action="" target="responseFrame4" method="post" enctype="multipart/form-data" name="submitForm4" id="submitForm4"> \
-            <p>instructionsXML</p> \
+            <p>instructionsXML:</p> \
             <input type="file" name="instructionsXML" /> \
             <button type="submit" form="submitForm4" onclick="redirectAction(document.submitForm4,\'?action=eoConfigureRoles\', parent.document.getElementById(\'hostname\').innerHTML)">submit</button> \
           </form> \
@@ -213,7 +214,7 @@ var eoAPI = {
         <div class="form"> \
           <h4>Parameters</h4> \
           <form action="" target="responseFrame5" method="post" enctype="multipart/form-data" name="submitForm5" id="submitForm5"> \
-            <p>instructionsXML</p> \
+            <p>instructionsXML:</p> \
             <input type="file" name="instructionsXML" /> \
             <button type="submit" form="submitForm5" onclick="redirectAction(document.submitForm5,\'?action=eoConfigureInvitation\', parent.document.getElementById(\'hostname\').innerHTML)">submit</button> \
           </form> \
@@ -259,7 +260,7 @@ var eoAPI = {
         <div class="form"> \
           <h4>Parameters</h4> \
           <form action="" target="responseFrame7" method="post" enctype="multipart/form-data" name="submitForm7" id="submitForm7"> \
-            <p>instructionsXML</p> \
+            <p>instructionsXML:</p> \
             <input type="file" name="instructionsXML" /> \
             <button type="submit" form="submitForm7" onclick="redirectAction(document.submitForm7,\'?action=eoGetAuthCode\', parent.document.getElementById(\'hostname\').innerHTML)">submit</button> \
           </form> \
@@ -274,7 +275,6 @@ var eoAPI = {
   ',
   //eoSecurityCheck
   eoSecurityCheck: ' \
-  <body onload="replaceEcoreWithSsweb();"> \
   <button class="accordion">eoSecurityCheck.eo</button> \
   <div class="panel"> \
     <div class="panelcontent"> \
@@ -292,13 +292,204 @@ var eoAPI = {
     </div> \
   </div> \
   </body> \
+  ',
+  //eoSellerInitiateTransfer
+  eoSellerInitiateTransfer: ' \
+  <button class="accordion">eoSellerInitiateTransfer</button> \
+  <div class="panel"> \
+    <div class="panelcontent"> \
+      <h4>Description</h4> \
+      <p>Creates a batch transfer of ownership request. This batch request may include one or many transactions. Each transaction described in the request, and its included documents, are locked and placed into escrow until the transfer of ownership is either confirmed by the \'seller\', cancelled by the \'seller\' or rejected by the \'buyer\'. The INITIATED_TRANSFER_OF_OWNERSHIP event is recorded for each document included in the request. A transfer receipt is created, signed by the \'seller\', and added to the escrow transaction. This transfer receipt will be included in the \'buyer\'s\' transaction upon completion of the transfer of ownership. An empty document profile is created in the \'seller\'s\' transaction to hold the transfer receipt signed by the \'buyer\' upon completion of the transfer of ownership.</p> \
+      <div class="apicontainer"> \
+        <div class="form"> \
+          <h4>Parameters</h4> \
+          <form action="" target="responseFrame8" method="post" enctype="multipart/form-data" name="submitForm8" id="submitForm8"> \
+            <p>instructionsXML:</p> \
+            <input type="file" name="instructionsXML" /> \
+            <button type="submit" form="submitForm8" onclick="redirectAction(document.submitForm8,\'?action=eoSellerInitiateTransfer\', parent.document.getElementById(\'hostname\').innerHTML)">submit</button> \
+          </form> \
+        </div> \
+        <div class="responsebox"> \
+          <h4>Response</h4> \
+          <iframe name="responseFrame8" class="response"></iframe> \
+        </div> \
+      </div> \
+    </div> \
+  </div> \
+  ',
+  //eoBuyerAcceptTransfer
+  eoBuyerAcceptTransfer: ' \
+  <button class="accordion">eoBuyerAcceptTransfer <span style="font-size:.8em">(API call in Buyer Vault)</span></button> \
+  <div class="panel"> \
+    <div class="panelcontent"> \
+      <h4>Description</h4> \
+      <p>Records the \'buyer\'s\' intent to accept control of the transactions/documents reflected in the transfer of ownership request. A transfer receipt is signed by the \'buyer\' and added to the escrow transaction. The ACCEPTED_TRANSFER_OF_OWNERSHIP event is recorded for each document included in the transfer request. \'Seller\' must next confirm transfer request before transfer of ownership completes.</p> \
+      <div class="apicontainer"> \
+        <div class="form"> \
+          <h4>Parameters</h4> \
+          <form action="" target="responseFrame9" method="post" enctype="multipart/form-data" name="submitForm9" id="submitForm9"> \
+            <input name="batchId" type="text" placeholder="batchId"/> \
+			<input name="manifestDPTName" type="text" placeholder="manifestDPTName"/> \
+            <button type="submit" form="submitForm9" onclick="redirectAction(document.submitForm9,\'?action=eoBuyerAcceptTransfer\', parent.document.getElementById(\'hostname\').innerHTML)">submit</button> \
+          </form> \
+        </div> \
+        <div class="responsebox"> \
+          <h4>Response</h4> \
+          <iframe name="responseFrame9" class="response"></iframe> \
+        </div> \
+      </div> \
+    </div> \
+  </div> \
+  ',
+  //eoSellerConfirmTransfer
+  eoSellerConfirmTransfer: ' \
+  <button class="accordion">eoSellerConfirmTransfer</button> \
+  <div class="panel"> \
+    <div class="panelcontent"> \
+      <h4>Description</h4> \
+      <p>For each transaction described in the transfer request, a new transaction is created in the \'buyer\'s\' organization. Control of each document included in the transfer is updated to reflect the \'buyer\' as the new controller/owner. The CONFIRMED_TRANSFER_OF_OWNERSHIP event is recorded for each transferred document. Retention policies described in the transfer request are executed for the \'seller\'. The transfer receipt, signed by the \'buyer\' is placed into the \'seller\'s\' transaction.</p> \
+      <div class="apicontainer"> \
+        <div class="form"> \
+          <h4>Parameters</h4> \
+          <form action="" target="responseFrame10" method="post" enctype="multipart/form-data" name="submitForm10" id="submitForm10"> \
+            <input name="batchId" type="text" placeholder="batchId"/> \
+            <button type="submit" form="submitForm10" onclick="redirectAction(document.submitForm10,\'?action=eoSellerConfirmTransfer\', parent.document.getElementById(\'hostname\').innerHTML)">submit</button> \
+          </form> \
+        </div> \
+        <div class="responsebox"> \
+          <h4>Response</h4> \
+          <iframe name="responseFrame10" class="response"></iframe> \
+        </div> \
+      </div> \
+    </div> \
+  </div> \
+  ',
+  //eoRequestDestruction
+  eoRequestDestruction: ' \
+  <button class="accordion">eoRequestDestruction</button> \
+  <div class="panel"> \
+    <div class="panelcontent"> \
+      <h4>Description</h4> \
+      <p>Uses a destruction instructions XML file to request the Destruction of entire transactions or individual Electronic Original® documents. If a transaction is requested to be destroyed, all Electronic Original documents in the transaction will be destroyed; all eStore or eCopy documents will be deleted; the empty transaction will be deleted upon authorization of the request. When a transaction is destroyed, no retention copies are permitted. To retain a copy of a destroyed Electronic Original document, the destruction request must be initiated against the document itself and not its transaction.</p> \
+      <div class="apicontainer"> \
+        <div class="form"> \
+          <h4>Parameters</h4> \
+          <form action="" target="responseFrame11" method="post" enctype="multipart/form-data" name="submitForm11" id="submitForm11"> \
+            <p>instructionsXML:</p> \
+            <input type="file" name="instructionsXML" /> \
+            <button type="submit" form="submitForm11" onclick="redirectAction(document.submitForm11,\'?action=eoRequestDestruction\', parent.document.getElementById(\'hostname\').innerHTML)">submit</button> \
+          </form> \
+        </div> \
+        <div class="responsebox"> \
+          <h4>Response</h4> \
+          <iframe name="responseFrame11" class="response"></iframe> \
+        </div> \
+      </div> \
+    </div> \
+  </div> \
+  ',
+  //eoAuthorizeDestruction
+  eoAuthorizeDestruction: ' \
+  <button class="accordion">eoAuthorizeDestruction</button> \
+  <div class="panel"> \
+    <div class="panelcontent"> \
+      <h4>Description</h4> \
+      <p>Uses a digital token to authorize a batch destruction request.</p> \
+      <div class="apicontainer"> \
+        <div class="form"> \
+          <h4>Parameters</h4> \
+          <form action="" target="responseFrame12" method="post" enctype="multipart/form-data" name="submitForm12" id="submitForm12"> \
+            <input name="batchId" type="text" placeholder="batchId"/> \
+            <button type="submit" form="submitForm12" onclick="redirectAction(document.submitForm12,\'?action=eoAuthorizeDestruction\', parent.document.getElementById(\'hostname\').innerHTML)">submit</button> \
+          </form> \
+        </div> \
+        <div class="responsebox"> \
+          <h4>Response</h4> \
+          <iframe name="responseFrame12" class="response"></iframe> \
+        </div> \
+      </div> \
+    </div> \
+  </div> \
+  ',
+  //eoGetCopy
+  eoGetCopy: ' \
+  <button class="accordion">eoGetCopy <span style="font-size:.8em">(Single Document)</span></button> \
+  <div class="panel"> \
+    <div class="panelcontent"> \
+      <h4>Description</h4> \
+      <p>Used to retrieve a watermarked copy of any document managed by eCore. Caller may specify watermark to apply, otherwise pre-configured system defaults will be applied based on the type of document being retrieved (eStored, electronicOriginal, or eCopy). The pre-configured system defaults are described in the defaultInstructions.xml as \'watermarkInstructions-eStore-retrieveDocument\', \'watermarkInstructions-electronicOriginal-retrieveDocument\', and \'watermarkInstructions-eCopy-retrieveDocument\'respectively.</p> \
+	  <div class="apicontainer"> \
+        <div class="form"> \
+          <h4>Parameters</h4> \
+          <form action="" target="responseFrame13" method="post" enctype="multipart/form-data" name="submitForm13" id="submitForm13"> \
+            <input name="dpSid" type="text" placeholder="dpSid"/> \
+            <button type="submit" form="submitForm13" onclick="redirectAction(document.submitForm13,\'?action=eoGetCopy\', parent.document.getElementById(\'hostname\').innerHTML)">submit</button> \
+          </form> \
+        </div> \
+        <div class="responsebox"> \
+          <h4>Response</h4> \
+          <iframe name="responseFrame13" class="response"></iframe> \
+        </div> \
+      </div> \
+    </div> \
+  </div> \
+  ',
+  //eoGetTransactionDocuments
+  eoGetTransactionDocuments: ' \
+  <button class="accordion">eoGetTransactionDocuments <span style="font-size:.8em">(All Documents in Transaction)</span></button> \
+  <div class="panel"> \
+    <div class="panelcontent"> \
+      <h4>Description</h4> \
+      <p>All document profiles associated with the transaction are collected and returned.</p> \
+	  <div class="apicontainer"> \
+        <div class="form"> \
+          <h4>Parameters</h4> \
+          <form action="" target="responseFrame14" method="post" enctype="multipart/form-data" name="submitForm14" id="submitForm14"> \
+            <input name="transactionSid" type="text" placeholder="transactionSid"/> \
+      			<p>verbose:</p> \
+      			<select name="verbose"> \
+      				<option value="NO" > \
+      					No \
+      				</option> \
+      				<option value="YES" selected="selected"> \
+      					Yes \
+      				</option> \
+      			</select> \
+      			<p>includeDocumentContent:</p> \
+      			<select name="includeDocumentContent"> \
+      				<option value="false" > \
+      					false \
+      				</option> \
+      				<option value="true" selected="selected"> \
+      					true \
+      				</option> \
+      			</select> \
+      			<p>concatDocumentContents:</p> \
+      			<select name="concatDocumentContents"> \
+      				<option value="false" > \
+      					false \
+      				</option> \
+      				<option value="true" selected="selected"> \
+      					true \
+      				</option> \
+      			</select> \
+            <button type="submit" form="submitForm14" onclick="redirectAction(document.submitForm14,\'?action=eoGetTransactionDocuments\', parent.document.getElementById(\'hostname\').innerHTML)">submit</button> \
+          </form> \
+        </div> \
+        <div class="responsebox"> \
+          <h4>Response</h4> \
+          <iframe name="responseFrame14" class="response"></iframe> \
+        </div> \
+      </div> \
+    </div> \
+  </div> \
   '
 }
 
 //Declare all workflow short name elements in an array to be used for html manipulation
 var workflows = ['embed','email','transfer','destruction','retrieve'];
 //Declare all API calls to insert HTML into
-var calls = ['.eoLogin', '.eoCreateTransactionUploadDocument', '.eoInsertFormFields', '.eoConfigureSortOrder', '.eoConfigureRoles', '.eoConfigureInvitation', '.eoLogout', '.eoGetAuthCode', '.eoSecurityCheck'];
+var calls = ['.eoLogin', '.eoCreateTransactionUploadDocument', '.eoInsertFormFields', '.eoConfigureSortOrder', '.eoConfigureRoles', '.eoConfigureInvitation', '.eoLogout', '.eoGetAuthCode', '.eoSecurityCheck', '.eoSellerInitiateTransfer', '.eoBuyerAcceptTransfer', '.eoSellerConfirmTransfer', '.eoRequestDestruction', '.eoAuthorizeDestruction', '.eoGetCopy', '.eoGetTransactionDocuments'];
 
 //Functions, make sure html classes/ids have the correct name otherwise these will not work as intended
 //
